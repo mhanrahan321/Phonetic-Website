1da324646aca5c9560ffe7f459cce94a619f7777
Mark Hanrahan <markhanrahan.1@gmail.com>
2024-01-19 12:38:06 -0500

    Returning HTML pages

--- index.js
+++ index.js
@@ -1,14 +1,19 @@
 const http = require("http");
+const fs = require('fs')
+const homePage = fs.readFileSync('index.html')
+const aboutPage = fs.readFileSync('about.html')
+const contactPage = fs.readFileSync('contact.html')
+const notFoundPage = fs.readFileSync('notfound.html')
 const server = http.createServer((req, res) => {
     if(req.url === '/about')
-        res.end("The about page")
+        res.end(aboutPage)
     else if(req.url==='/contact')
-        res.end("The contact page")     
+        res.end(contactPage)     
     else if(req.url==='/')
-        res.end("The home page")
+        res.end(homePage)
     else{
         res.writeHead(404)
-        res.end('page not found')
+        res.end(notFoundPage)
     }
 })
 server.listen(3000);
