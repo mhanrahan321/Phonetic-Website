9a1637af0de5ee8c254f1b4df1821da35df58f74
Mark Hanrahan <markhanrahan.1@gmail.com>
2024-01-22 15:20:41 -0500

    NewBlogApp routing

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 const express = require('express')
-const path = require ('path')
+const path = require('path')
+
 const app = express()
 app.use(express.static('public'))
+
 app.listen(3000, ()=>{
     console.log("App listening on port 3000")
 })
 
-app.get("/",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'index.html'))       
-    })
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'pages/index.html'))
+})
 
-app.get("/about",(req,res)=>{  
-    res.sendFile(path.resolve(__dirname,'about.html'))
-    })
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'pages/contact.html'))
+})
 
-app.get("/contact",(req,res)=>{  
-    res.sendFile(path.resolve(__dirname,'contact.html'))
-    })
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'pages/post.html'))
+})
 
-app.get('/*',(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'notfound.html'))
-    }) //test this
+app.get('/',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'pages/about.html'))
+})
--- public/js/index.js
+++
@@ -1,1 +0,0 @@
-alert   ('local js file');
