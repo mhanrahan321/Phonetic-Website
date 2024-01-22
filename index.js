047793fed838ca99968b3ca4439a1676d9fe1865
Mark Hanrahan <markhanrahan.1@gmail.com>
2024-01-22 11:07:13 -0500

    added public folder

--- index.js
+++ index.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const path = require ('path')
 const app = express()
+app.use(express.static('public'))
 app.listen(3000, ()=>{
     console.log("App listening on port 3000")
 })
 
 app.get("/",(req,res)=>{
-    res.sendFile(path.resolve(__dirname,'index.html'))
-        name:'HOME PAGE'
+    res.sendFile(path.resolve(__dirname,'index.html'))       
     })
 
 app.get("/about",(req,res)=>{  
     res.sendFile(path.resolve(__dirname,'about.html'))
-        name:'About us'
     })
+
+app.get("/contact",(req,res)=>{  
+    res.sendFile(path.resolve(__dirname,'contact.html'))
+    })
+
+app.get('/*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'notfound.html'))
+    }) //test this
