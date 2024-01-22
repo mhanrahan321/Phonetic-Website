390743c254925101088496b86564c3cc035d72a1
Mark Hanrahan <markhanrahan.1@gmail.com>
2024-01-22 10:19:21 -0500

    added path

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
-const http = require("http");
-const fs = require('fs')
-const homePage = fs.readFileSync('index.html')
-const aboutPage = fs.readFileSync('about.html')
-const contactPage = fs.readFileSync('contact.html')
-const notFoundPage = fs.readFileSync('notfound.html')
-const server = http.createServer((req, res) => {
-    if(req.url === '/about')
-        res.end(aboutPage)
-    else if(req.url==='/contact')
-        res.end(contactPage)     
-    else if(req.url==='/')
-        res.end(homePage)
-    else{
-        res.writeHead(404)
-        res.end(notFoundPage)
-    }
+const express = require('express')
+const path = require ('path')
+const app = express()
+app.listen(3000, ()=>{
+    console.log("App listening on port 3000")
 })
-server.listen(3000);
+
+app.get("/",(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'index.html'))
+        name:'HOME PAGE'
+    })
+
+app.get("/about",(req,res)=>{  
+    res.sendFile(path.resolve(__dirname,'about.html'))
+        name:'About us'
+    })
